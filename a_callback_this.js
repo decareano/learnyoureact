ee797963f61ee42f09c32e1df1868cb6b5631ee4
decareano <Marcelo Gobelli>
2016-07-01 19:01:33 -0700

    first commit

---
+++ a_callback_this.js
@@ -0,0 +1,11 @@
+var clientData = {
+    id: 094545,
+    fullName: "Not Set",
+    setUserName: function(firstName, lastName) {
+        this.fullName = firstName + " " + lastName;
+
+    }
+}
+function getUserInput(firstName, lastName, callback) {
+    callback(firstName, lastName);
+}
